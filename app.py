7d7d98e718e4c137c1c1ad717871f7624153c5c4
lfernandez55 <lfernandez@weber.edu>
2020-02-10 20:26:21 -0700

    Commit

--- app.py
+++ app.py
@@ -1,29 +1,15 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
-from flask_wtf import FlaskForm #Form is deprecated, use FlaskForm instead
-from wtforms import StringField, IntegerField, ValidationError, FieldList, FormField, SubmitField, HiddenField, SelectMultipleField, widgets
-from wtforms.validators import InputRequired
 
 app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
-db = SQLAlchemy(app)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000, debug=True)
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-@app.route('/create_all')
-def create_all():
-    db.create_all()
-    flash('DB Added!!')
-    return redirect(url_for('home_page'))
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
 
-@app.route('/drop_all')
-def drop_all():
-    db.drop_all()
-    flash('DB Dropped!!')
-    return redirect(url_for('home_page'))
+
